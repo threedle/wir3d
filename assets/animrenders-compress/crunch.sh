b4c37ac1d5b78dbb7fc931090956edf2bfb38750
factoryofthesun <guanzhi@uchicago.edu>
2025-04-30 20:59:11 -0500

    website big upload

--- assets/animrenders-compress/crunch.sh
+++
@@ -1,6 +0,0 @@
-for fname in "$@"
-do
-    outfname="$(basename "$fname")"
-    echo "$fname -> $outfname"
-    ffmpeg -y -i "$fname" -c:v libx264 -vf "scale=iw*0.84:-1" -crf 26 -preset veryslow -tune animation -movflags faststart "$outfname"
-done
